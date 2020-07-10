7ab09190bad2774a41607adffe66d4d1a8c98fe1
luisits <lalcala@itsmarts.com.mx>
2020-07-10 18:29:06 -0500

    Se agregan interfaces

---
+++ src/app/interfaces/producto.ts
@@ -0,0 +1,8 @@
+export interface Producto {
+    descripcion: string;
+    inventario: number;
+    nombre: string;
+    precio: number;
+    tipo: string;
+    urlFoto: string;
+}
---
+++ src/app/interfaces/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    nombre: string;
+    correo: string;
+    usuario: string;
+    contraseña: string;
+    rol: 'usuario';
+}
